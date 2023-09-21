c485da8f52169454137dd22538b511173d6b8e69
Ritik <ritikbansal621@gmail.com>
2023-09-21 22:39:52 +0530

    Updates

---
+++ precache-manifest.756cb7e50460efb2b21c66d24643d37d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c12950ecba580bc0088",
+    "url": "/Ritik216/TextUtils-React/static/css/main.df163b83.chunk.css"
+  },
+  {
+    "revision": "0c12950ecba580bc0088",
+    "url": "/Ritik216/TextUtils-React/static/js/main.0c12950e.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/Ritik216/TextUtils-React/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "4fbba0e190d7f44914e8",
+    "url": "/Ritik216/TextUtils-React/static/js/2.4fbba0e1.chunk.js"
+  },
+  {
+    "revision": "cfbc0ad170aef9f227c6",
+    "url": "/Ritik216/TextUtils-React/static/js/runtime~main.cfbc0ad1.js"
+  },
+  {
+    "revision": "316fae23668bbdaf701035ce8bef6af5",
+    "url": "/Ritik216/TextUtils-React/index.html"
+  }
+];
